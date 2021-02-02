d86696ef77272a4bab8e175bef064b62df21b91d
jakecalub <jakecalub28@gmail.com>
2021-02-03 08:28:27 +1100

    foa

--- src/Menu.java
+++ src/Menu.java
@@ -34,6 +34,32 @@ public class Menu {
 
     //produce a text file that contains all events from an input machine
     public void makeFileOfMachineEvents(String machine, HashMap<String, ArrayList<Event>> events) {
+        FileOutputStream fos;
+        ObjectOutputStream oos;
+
+        try {
+            fos = new FileOutputStream(machine + "-report.txt");
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject("Events for machine '" + machine + "'\n");
+            for(Event ev: events.get(machine)) {
+                if (ev.getEventType().equalsIgnoreCase("INVENTORY")) {
+                    Inventory iEv = (Inventory) ev;
+                    oos.writeObject(iEv.getEventTime() + " " + iEv.getMachineName() + " " + iEv.getEventType() + " " + iEv.getInventoryType() + " " + iEv.getInventoryStatus() + "\n");
+                }
+                else if (ev.getEventType().equalsIgnoreCase("POLICY")) {
+                    Policy pEv = (Policy) ev;
+                    oos.writeObject(pEv.getEventTime() + " " + pEv.getMachineName() + " " + pEv.getEventType() + " " + pEv.getPolicyId() + " " + pEv.getPolicyStatus() + "\n");
+                }
+                else if (ev.getEventType().equalsIgnoreCase("SOFTWAREUPDATES")) {
+                    SoftwareUpdate sEv = (SoftwareUpdate) ev;
+                    oos.writeObject(sEv.getEventTime() + " " + sEv.getMachineName() + " " + sEv.getEventType() + " " +  sEv.getSoftwareUpdateId() + " " + sEv.getSoftwareUpdateStatus() + "\n");
+                }
+            }
+            oos.writeObject("End of events.");
+        }
+        catch (Exception e) {
+            System.out.println("There was an ERROR.");
+        }
     }
 
     //check all events on the logfile and display all failed events
